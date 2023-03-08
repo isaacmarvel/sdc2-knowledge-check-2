475e08cdea6b7d1816e9a92668eb2fc6ecd97ecf
isaacmarvel <isaac.marvel.77@gmail.com>
2023-03-08 18:29:52 -0500

    Added queries

---
+++ Software-2-Knowledge-Check-2.sql
@@ -0,0 +1,9 @@
+﻿Select Customers.CustomerName, Customers.CustomerID, Orders.OrderID
+From Orders
+Inner join Customers on Orders.CustomerID=Customers.CustomerID
+where OrderID=10310
+
+Select Products.ProductID, Products.SupplierID, Suppliers.Address
+From Products
+Inner join Suppliers on Products.SupplierID= Suppliers.SupplierID
+where ProductID=40
